f80a3f25075728000aec2b53624c66b5ccfd9fd4
kartikmavani <kartikmavani@gmail.com>
2021-06-23 00:06:17 +1000

    Dummy commit

--- src/sample-api/src/main/java/com/demo/api/sampleapi/SampleApiApplication.java
+++ src/sample-api/src/main/java/com/demo/api/sampleapi/SampleApiApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SampleApiApplication {
 
 	public static void main(String[] args) {
+		System.out.println("----------a----------");
 		SpringApplication.run(SampleApiApplication.class, args);
 	}
 
